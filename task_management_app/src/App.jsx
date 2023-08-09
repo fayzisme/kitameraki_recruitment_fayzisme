c1df98e40dafcca59d084eb48eeb975b22affca6
Aneis Me <ammadfayz@gmail.com>
2023-08-09 10:43:51 +0700

    remove unused komponen'

--- task_management_app/src/App.jsx
+++ task_management_app/src/App.jsx
@@ -2,13 +2,11 @@ import { useState, useEffect } from 'react'
 import Header from "./components/Header";
 import Content from './components/Content';
 import Footer from './components/Footer';
-import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <>
       <Header/>
-      <Sidebar/>
       <Content/>
       <Footer className='footer'/>
     </>
